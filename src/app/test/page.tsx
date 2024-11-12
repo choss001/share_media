590f9a8c91cb1053bf2c0420b6ed2aac5c0f2e50
choss001 <choss01@naver.com>
2024-11-12 14:03:00 +0900

    upload media

---
+++ src/app/test/page.tsx
@@ -0,0 +1,53 @@
+'use client'
+import { useEffect, useState} from 'react';
+
+export default function page(){
+    const [selectedFile, setSelectedFile] = useState<File | null>(null);
+    const [uploadStatus, setUploadStatus] = useState("");
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files == null){
+            console.error("file is null");
+            return null;
+        }
+
+        setSelectedFile(event.target.files[0]);
+    };
+    const handleFileUpload = async () => {
+        if (!selectedFile) {
+            alert("Please select a file first!");
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append("file", selectedFile);
+
+        try {
+            const response = await fetch("http://localhost:8080/upload/media", {
+                method: "POST",
+                body: formData,
+            });
+
+            if (response.ok) {
+                const downloadUrl = await response.text();
+                setUploadStatus(`File uploaded successfully! Access it here: ${downloadUrl}`);
+            } else {
+                setUploadStatus("Failed to upload file.");
+            }
+        } catch (error) {
+            console.error("Error uploading file:", error);
+            setUploadStatus("Error uploading file.");
+        }
+    };
+
+    return (
+        <>
+            <div>
+                <h1>Upload Media File</h1>
+                <input type="file" onChange={handleFileChange} />
+                <button onClick={handleFileUpload}>Upload</button>
+                <p>{uploadStatus}</p>
+            </div>
+
+        </>
+    )
+}
