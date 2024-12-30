df80d459eb607391dea953ee4c3d14a5689e6689
choss001 <choss01@naver.com>
2024-12-30 10:37:50 +0900

    react icons added

--- src/app/topic/create/page.tsx
+++ src/app/topic/create/page.tsx
@@ -1,17 +1,38 @@
-'use server'
+'use client'
 import Image from "next/image";
 
 export default function Home() {
   function formAction(){
-
+    return
   }
+
+  const customers = [{id:1, value:"test1"}, {id:2, value:"test2"},{id:3, value:"test3"} ]
   return (
-    <div className="flex-grow p-6 h-screen">
-      <div className="flex bg-gray-50 w-min-full items-center">
-        <form action={formAction}>
-        </form>
-        
+    <form>
+      <div className="p-6 h-screen">
+        <div className="bg-gray-50 w-min-full p-6 items-center">
+          <label className="mb-2 block text-sm font-medium">
+            Choose customer
+          </label>
+          <select
+            id="customer"
+            name="customerId"
+            className=""
+            defaultValue=""
+            aria-describeby="customer-error"
+          >
+            <option value="" disabled>
+              Select a customer
+
+            </option>
+            {customers.map((customer) => (
+              <option key={customer.id}>
+                {customer.value}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
-    </div>
+    </form>
   );
 }
