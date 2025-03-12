f02685acb203d334c0b2dbb5f2f19da04f987278
choss001 <choss01@naver.com>
2025-03-12 21:51:45 +0900

    modify source code because the error that useSearchParams have to be warraped suspense

--- src/app/topic/components/pagination.tsx
+++ src/app/topic/components/pagination.tsx
@@ -3,22 +3,7 @@
 import { usePathname, useSearchParams } from 'next/navigation';
 import { Suspense } from 'react';
 
-export default function Pagination({totalPage }: {totalPage: number }) {
-    const pathname = usePathname();
-    console.log(`pathname!!!! ${pathname}`);
-    const searchParams = useSearchParams();
-    console.log(`searchParams!!!! ${searchParams}`);
-    const currentPage = Number(searchParams.get('page')) || 1;
-    console.log(`currentPage!!!! ${currentPage}`);
-    const createPageURL = (pageNumber: number | string) => {
-        const params = new URLSearchParams(searchParams);
-        console.log(`parmas!!!! ${params}`);
-        params.set('page', pageNumber.toString());
-        return `${pathname}?${params.toString()}`;
-    }; 
-    console.log(`createPageURL!!!! ${createPageURL(2)}`);
-
-
+export default function Pagination({ totalPage }: { totalPage: number }) {
     return (
         <Suspense fallback={<div>Loading...</div>}>
             <PaginationContent totalPage={totalPage} />
