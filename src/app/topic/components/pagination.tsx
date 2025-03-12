ad7c7bfacf1a63c34a850f43a4cf8ff56da18323
choss001 <choss01@naver.com>
2025-03-12 21:44:55 +0900

    using suspanse becuase can't start docker-compose file

--- src/app/topic/components/pagination.tsx
+++ src/app/topic/components/pagination.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { usePathname, useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
 export default function Pagination({totalPage }: {totalPage: number }) {
     const pathname = usePathname();
@@ -19,8 +20,29 @@ export default function Pagination({totalPage }: {totalPage: number }) {
 
 
     return (
-        <div>
-            {totalPage}
-        </div>
+        <Suspense fallback={<div>Loading...</div>}>
+            <PaginationContent totalPage={totalPage} />
+        </Suspense>
     );
+}
+
+function PaginationContent({ totalPage }: { totalPage: number }) {
+    const pathname = usePathname();
+    console.log(`pathname!!!! ${pathname}`);
+
+    const searchParams = useSearchParams();
+    console.log(`searchParams!!!! ${searchParams}`);
+
+    const currentPage = Number(searchParams.get('page')) || 1;
+    console.log(`currentPage!!!! ${currentPage}`);
+
+    const createPageURL = (pageNumber: number | string) => {
+        const params = new URLSearchParams(searchParams);
+        console.log(`parmas!!!! ${params}`);
+        params.set('page', pageNumber.toString());
+        return `${pathname}?${params.toString()}`;
+    };
+    console.log(`createPageURL!!!! ${createPageURL(2)}`);
+
+    return <div>{totalPage}</div>;
 }
