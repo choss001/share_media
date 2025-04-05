9255c0f18fc11b86a1f86daa4c2de9dd30455bd0
choss001 <choss01@naver.com>
2025-04-06 06:02:50 +0900

    commit before i modify llm page

--- src/app/test/hlsTest/[fileNameUid]/page.tsx
+++ src/app/test/hlsTest/[fileNameUid]/page.tsx
@@ -8,22 +8,26 @@ export default function Home({ params }: {params: {fileNameUid: string}}){
     const fileNameUid = params.fileNameUid;
     const apiUrl = process.env.NEXT_PUBLIC_SPRING_API_URL;
     const videoRef = useRef<HTMLVideoElement>(null)
-    const source = `${apiUrl}/resource/media/hls/${fileNameUid}/master.m3u8`
+    // const source = `${apiUrl}/resource/media/hls/${fileNameUid}/master.m3u8`
+    const source = `http://localhost:81/static/hls/${fileNameUid}/master.m3u8`
 
     useEffect(() => {
-        const hls = new Hls()
+        const hls = new Hls({
+            xhrSetup: function (xhr) {
+              xhr.withCredentials = true; // ✅ send cookies!
+            }
+          });
         if(Hls.isSupported()){
             hls.loadSource(source)
             if(videoRef.current){
                 hls.attachMedia(videoRef.current)
             }
         }
-
     },[])
     return (
         <>
             <div className="mt-[50px]">
-                <video controls ref={videoRef}/>
+                <video controls crossOrigin="use-credentials" ref={videoRef}/>
             </div>
 
         </>
